fc647a53485bd197762a7e03d38d0d658696e626
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-03-30 00:53:06 +0600

    added the getstarted section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Banner from './components/banner/Banner'
+import Cards from './components/cards/Cards'
 import DigitalTools from './components/digitalTools/DigitalTools'
+import GetStarted from './components/getstarted/Getstarted'
 import Navbar from './components/navbar/Navbar'
 import State from './components/state/State'
 
@@ -13,6 +15,8 @@ function App() {
       <Banner/>
       <State/>
       <DigitalTools/>
+      <Cards/>
+      <GetStarted/>
     </>
   )
 }
