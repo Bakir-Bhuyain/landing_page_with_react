d3372f2e6daede610b244c5bfc50bd9064a01f82
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-03-30 12:46:22 +0600

    added the cards dynamically using the map

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import './App.css'
 import Banner from './components/banner/Banner'
 import Cards from './components/cards/Cards'
@@ -12,13 +13,25 @@ import Subscription from './components/subscription/Subscription'
 
 function App() {
 
+  const fetchedCardInfo = async() =>{
+    const res = await fetch('/Features.json');
+    return res.json();
+  }
+
+const promiseCardInfo = fetchedCardInfo();
+
   return (
     <>
       <Navbar/>
       <Banner/>
       <State/>
       <DigitalTools/>
-      <Cards/>
+      
+      <Suspense fallback={<span className="$$loading $$loading-spinner $$loading-xl"></span>}>
+        <Cards promiseCardInfo={promiseCardInfo} />
+      </Suspense>
+
+
       <GetStarted/>
       <Subscription/>
       <Transform/>
